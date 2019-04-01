69c82aefb71b81a60324de770e841a11b8b0445e
clem9281 <clem9281@gmail.com>
2019-04-01 14:03:27 -0700

    fixed my login endpoint, i was using the headers by accident

--- routers/login.js
+++ routers/login.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcryptjs");
 const router = express.Router();
 
 router.route("/").post(async (req, res) => {
-  const { username, password } = req.headers;
+  const { username, password } = req.body;
   if (!username || !password) {
     return res.status(400).json({ error: "Bad request" });
   }
